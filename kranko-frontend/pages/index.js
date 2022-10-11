bdd91696dec86187e1f9655e446ac494ac6d6f62
Samuel wanza <s.wanza@alustudent.com>
2022-10-11 11:32:23 +0200

    project structure

--- kranko-frontend/pages/index.js
+++ kranko-frontend/pages/index.js
@@ -1,13 +1,24 @@
+import React from "react";
 import Head from 'next/head'
 import Image from 'next/image'
+import { Navbar,HeroSection,CallToAction,AboutUs,Mission,Footer, Testimonials} from '../src/Components/mainPageComponents'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="">
-      <h1 className='ml-10'>KrankoJobs</h1>
+    <React.Fragment>
+      <div className="bg-blue-300">
+        <Navbar/>
+        <HeroSection/>
+        <AboutUs/>
+        <Mission/>
+        <Testimonials/>
+        <CallToAction/>
+        <Footer/>
 
-      
-    </div>
+
+        
+      </div>
+    </React.Fragment>
   )
 }
