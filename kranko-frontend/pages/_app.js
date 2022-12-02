4525e65e11494b07fd22e2eaf77605323ab9402a
Samuel wanza <s.wanza@alustudent.com>
2022-12-02 21:14:27 +0200

    more backend setup

--- kranko-frontend/pages/_app.js
+++ kranko-frontend/pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ApolloProvider } from "@apollo/client";
+import "../styles/globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import UserProvider from "../src/context/UserContext";
+import { useApollo } from "../src/lib/apolloClient";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const client = useApollo(pageProps);
+
+  return (
+    <ApolloProvider client={client}>
+      <UserProvider>
+        <Component {...pageProps} />;
+        <ToastContainer />
+      </UserProvider>
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
