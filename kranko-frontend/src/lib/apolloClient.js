a0c1d6307156092ecfe68841af92fa3c45d820a1
Samuel wanza <s.wanza@alustudent.com>
2023-03-23 23:41:52 +0200

    changing backend url

--- kranko-frontend/src/lib/apolloClient.js
+++ kranko-frontend/src/lib/apolloClient.js
@@ -11,7 +11,7 @@ export const APOLLO_STATE_PROP_NAME = "__APOLLO_STATE__";
 let apolloClient;
 
 const httpLink = new HttpLink({
-  uri: "https://krankojobs-backend.up.railway.app/graphql", // Server URL (must be absolute)
+  uri: "https://backend-production-e488.up.railway.app/graphql", // Server URL (must be absolute)
 });
 
 const authLink = setContext((_, { headers }) => {
